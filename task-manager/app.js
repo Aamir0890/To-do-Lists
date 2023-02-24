79c7f8a1ef3c6e9d5936802f6ffa99d20bdaa243
Yourdad404 <arshadaamir09@gmail.com>
2023-02-24 13:07:54 +0530

    first commit

---
+++ task-manager/app.js
@@ -0,0 +1,31 @@
+
+const express=require('express')
+const app=express();
+
+const tasks=require('./routes/task')
+const connectDB=require('./db/connect')
+require ('dotenv').config();
+
+//middleware
+app.use(express.static('./public'))
+
+app.use(express.json())
+
+
+app.use('/api/v1/tasks',tasks)
+
+
+const port=3000;
+
+
+const start=async()=>{
+    try{
+     await connectDB(process.env.MONGO_URI)
+     app.listen(port,console.log('server is listening'))
+    }
+    catch(err){
+    console.log(err)
+    }
+}
+start();
+
